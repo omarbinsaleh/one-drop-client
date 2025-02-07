708069cb1f7372d83d4d8bcccbe99354a95114d1
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-02-07 20:41:47 +0400

    reformated the DonorInfo conponent

--- src/components/DonorInfo.jsx
+++ src/components/DonorInfo.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const DonorInfo = ({donorInfo, status}) => {
-  return (
-   <div className='capitalize'>
-   <h3>Name: {status === 'done' ? <span>{donorInfo.name}</span> : <span className='uppercase text-slate-500'>N/A</span>}</h3>
-   <h3>Email: {status === 'done' ? <span className='lowercase text-slate-500'>{donorInfo.email}</span> : <span className='uppercase text-slate-500'>N/A</span>}</h3>
-</div>
-  )
+const DonorInfo = ({ donorInfo, status }) => {
+   return (
+      <div className='capitalize'>
+         <h3>Name: {status === 'done' ? <span>{donorInfo.name}</span> : <span className='uppercase text-slate-500'>N/A</span>}</h3>
+         <h3>Email: {status === 'done' ? <span className='lowercase text-slate-500'>{donorInfo.email}</span> : <span className='uppercase text-slate-500'>N/A</span>}</h3>
+      </div>
+   )
 }
 
 export default DonorInfo
