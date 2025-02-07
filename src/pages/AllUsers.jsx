5406c6680f212548c66e1ba0718f5e44818b4c1c
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-02-07 22:55:08 +0400

    added the page title and a filtering option in the AllUsers page

--- src/pages/AllUsers.jsx
+++ src/pages/AllUsers.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../components/Title'
+import useAllUsers from '../hooks/useAllUsers';
 
 const AllUsers = () => {
-  return (
-    <div>
-      All users page
-    </div>
-  )
+   const [filter, setFilter] = useState('');
+
+   const {isPending, data, error, isFetching, refetch} = useAllUsers({userStatus: filter})
+   
+   const handleFilter = (e) => {
+      setFilter(e.target.value)
+   };
+
+   return (
+      <section className='py-8 pt-2 max-h-screen flex flex-col w-full'>
+         <Title title='All Users'></Title>
+
+         <div className='flex items-center  sm:justify-end  gap-3 flex-wrap px-1'>
+            <select onChange={handleFilter} value={filter} className='select select-md rounded-none border border-gray-300 bg-secondary text-white'>
+               <option value="">Filter by Status</option>
+               <option value="active">Active</option>
+               <option value="blocked">Blocked</option>
+            </select>
+         </div>
+
+
+
+      </section>
+   )
 }
 
 export default AllUsers
