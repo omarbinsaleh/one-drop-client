df29d0a88cdfd070a3591a7c85858116e4faa74c
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-02-07 22:43:09 +0400

    modified the useAllUsers hook again

--- src/hooks/useAllUsers.jsx
+++ src/hooks/useAllUsers.jsx
@@ -1,7 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios';
-import React from 'react'
-import { data } from 'react-router-dom';
 
 const useAllUsers = ({userStatus=''}) => {
   const query = useQuery({
@@ -11,17 +9,17 @@ const useAllUsers = ({userStatus=''}) => {
 
       // FETCH ALL USERS DATA
       try {
-         const users = await axios.get(`${import.meta.env.VITE_API_URL}/users?status=${userStatus}`)
-         result.users = {data: users, success:true, message: 'Data is retured successfully' }
+         const {data:allUsersData} = await axios.get(`${import.meta.env.VITE_API_URL}/users?status=${userStatus}`)
+         result.users = {data: allUsersData, success:true, message: 'Data is retured successfully' }
       } catch (error) {
-         result.users = {data: [], success: false, message: 'Something went wrong' }
+         result.users = {data: [], success: false, message: 'Something went wrong', error }
       }
 
       return result;
    }
-  })
+  });
 
   return query;
 }
 
-export default useAllUsers
+export default useAllUsers;
